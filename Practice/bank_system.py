fd534c332c813b5b18e9a0389e5a58b9a79b3a55
TRIC1A <triciagalejardiel8@gmail.com>
2026-04-08 23:23:43 +0800

    Created more clean structure of the system and edited some line of codes to improve the system

--- Practice/bank_system.py
+++ Practice/bank_system.py
@@ -4,12 +4,12 @@
 # 3. Show balance
 
 # flow
-# 1. ask the user a choice [1-4]
+# 1. ask the user a choice
 # 2. in deposit 
 #   > ask user how much money need to deposit(limit: 100000)
 # 3. in withdraw 
-#   > ask user how much money he will withdraw
-#   > check is the withdraw is less that whats in the balance
+#   > ask user how much money to withdraw
+#   > check if the withdraw is less than whats in the balance
 # 4. in show balance
 #   > show how much balance the user currently have
 #   > create a variables to store the balance money of the user
@@ -51,13 +51,14 @@
 
 # import mysql.connector
 import random
+from datetime import datetime
 
 
 accounts = {
-    1111: {"name": "tricia", "balance": 0, "transactions": []},
-    1122: {"name": "gale", "balance": 0, "transactions": []},
-    2222: {"name": "kit", "balance": 0, "transactions": []},
-    2233: {"name": "kae", "balance": 0, "transactions": []}
+    1111: {"name": "tricia", "pin": "1234", "balance": 0, "transactions": []},
+    1122: {"name": "gale", "pin": "5678", "balance": 0, "transactions": []},
+    2222: {"name": "kit", "pin": "4321", "balance": 0, "transactions": []},
+    2233: {"name": "kae", "pin": "8765", "balance": 0, "transactions": []}
 }
 
 
@@ -86,40 +87,60 @@ def user_login():
     account_name = input("\nAccount Name: ")
     account_number = int(input("Account Number: "))
     if account_number in accounts and accounts[account_number]["name"] == account_name:
-        print("Login successful")
-        bank_menu(account_number)   # pass only the account number
+        if authenticate(account_number):
+            print("Login successful")
+            bank_menu(account_number)   # pass only the account number
+        else:
+            print("Wrong PIN. Access denied.")
     else:
         print("Wrong account name or account number")
             
+
+#  get the current time
+def get_current_time():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+def authenticate(account_id):
+    entered_id = input("Enter PIN: ")
+    return accounts[account_id]["pin"] == entered_id
     
 # bank menu of the user
 def bank_menu(sender_id):
     acc = accounts[sender_id]   # get the account dictionary
     
-    # added timestamps when the transfer happened
-    from datetime import datetime
-    transaction_id = random.randint(100000, 999999)
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    
     while True:
-        print(f"\n========== Welcome User: {acc["name"]} ==========")
+        print(f"\n========== Welcome User: {acc['name']} ==========")
         print("[1] Deposit")   
         print("[2] Withdraw")
         print("[3] Transfer Money")
         print("[4] View Balance")
         print("[5] Transaction History")
         print("[6] Logout")
-        choice = int(input("Choose [1-6]: "))
+        try:
+            choice = int(input("Choose [1-6]: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
         
         match(choice):
             case 1:     # Deposit
-                amount = int(input("\nEnter amount: "))
+                try:
+                    amount = int(input("\nEnter amount: "))
+                except ValueError:
+                    print("Invalid input. Please enter a number.")
+                    continue
+                
                 if amount <= 0:
                     print("Invalid amount.")
                 elif amount > 100000:
                     print("You exceed the limit to deposit: ")
                 else:
                     acc['balance'] += amount
+                    
+                    # added timestamps when the transfer happened
+                    transaction_id = random.randint(100000, 999999)
+                    timestamp = get_current_time()
+                    
                     acc["transactions"].append({
                         "id": transaction_id,
                         "type": "deposit", 
@@ -129,7 +150,17 @@ def bank_menu(sender_id):
                     print(f"\nDeposited: {amount}\nNew Balance: {acc['balance']:.2f}")
                     
             case 2:     # Withdraw
-                withdraw = int(input("\nEnter amount: "))
+                
+                if not authenticate(sender_id):
+                    print("Authentication failed. Wrong PIN.")
+                    continue
+                
+                try:
+                    withdraw = int(input("\nEnter amount: "))
+                except ValueError:
+                    print("Invalid input. Please enter a number.")
+                    continue
+                
                 if withdraw > acc['balance']:
                     print("Insuficient money.")
                     print(f"Your current balance: {acc['balance']:.2f}")
@@ -138,6 +169,11 @@ def bank_menu(sender_id):
                     print(f"You must maintain at least 500 balance.")
                 else:
                     acc['balance'] -= withdraw
+                    
+                    # added timestamps when the transfer happened
+                    transaction_id = random.randint(100000, 999999)
+                    timestamp = get_current_time()
+                    
                     acc["transactions"].append({
                         "id": transaction_id,
                         "type": "withdraw", 
@@ -148,10 +184,20 @@ def bank_menu(sender_id):
             
             case 3:     # Transfer Money
                 """ Transfer Money """
-                recipient_id = int(input("Enter Recipient Account Number: "))   
+                if not authenticate(sender_id):
+                    print("Authentication failed. Wrong PIN.")
+                    continue
+                
+                try:
+                    recipient_id = int(input("Enter Recipient Account Number: "))  
+                except ValueError:
+                    print("Invalid input. Please enter a number.")
+                    continue
+                 
                 
                 if recipient_id == sender_id:
                     print("You cannot transfer to your own account.")
+                    continue
                             
                 if recipient_id in accounts:
                     transfer_amount = int(input("Enter amount: "))
@@ -162,7 +208,13 @@ def bank_menu(sender_id):
                         print("Cannot transfer, maintaining balance requirement not met.")
                     else:
                         choice = input("Confirm transfer? [Y/N]: ").upper()
+                        
                         if choice == "Y":
+                            
+                            
+                            transaction_id = random.randint(100000, 999999)
+                            timestamp = get_current_time()      # added timestamps when the transfer happened
+                            
                             acc['balance'] -= transfer_amount
                             accounts[recipient_id]['balance'] += transfer_amount
                             
@@ -195,26 +247,37 @@ def bank_menu(sender_id):
                 if not acc["transactions"]:
                     print(("No transaction yet."))
                 else:
-                    print(f"{'ID':<8}{'TYPE':<16}{'DETAILS':<30}{'AMOUNT':<12}{'DATE'}")
-                    print("-" * 75)
+                    print("-" * 95)
+                    print(f"{'ID':<10}{'TYPE':<20}{'DETAILS':<30}{'AMOUNT':<15}{'DATE'}")
+                    print("-" * 95)
                     
                     for tran in acc["transactions"]:
-                        if tran["type"] == "deposit":
-                            # print(f"    + Deposit         : {tran['amount']:.2f}")
-                            print(f"[ID:{tran['id']}] + Deposit : {tran['amount']:.2f} | {tran['time']}")
-                        elif tran["type"] == "withdraw":
-                            # print(f"    - Withdraw        : {tran['amount']:.2f}")
-                            print(f"[ID:{tran['id']}] - Withdraw : {tran['amount']:.2f} | {tran['time']}")
-                        elif tran["type"] == "transfer_out":       
-                            recipient_id = tran["to"]   # account number stored in transaction                  
-                            timestamp = tran["time"]    # fetch the time from transaction
-                            # print(f"    -> Transfer Out   : {tran['amount']:.2f} to {recipient_id} ({accounts[recipient_id]['name']})\tDate: {timestamp}")
-                            print(f"[ID:{tran['id']}] -> Transfer Out : {tran['amount']:.2f} to {recipient_id} ({accounts[recipient_id]['name']}) | {timestamp}")
-                        elif tran["type"] == "transfer_in":
-                            sender_id = tran["from"]    # account number stored in transaction
-                            timestamp = tran["time"]    # fetch the time from transaction
-                            # print(f"    <- Transfer In   : {tran['amount']:.2f} from {sender_id} ({accounts[sender_id]['name']})\tDate: {timestamp}")
-                            print(f"[ID:{tran['id']}] <- Transfer In : {tran['amount']:.2f} from {sender_id} ({accounts[sender_id]['name']}) | {timestamp}")
+                        t_id = tran["id"]
+                        t_type = tran["type"]
+                        amount = tran["amount"]
+                        time = tran["time"]
+                        
+                        if t_type == "deposit":
+                            details = "Cash Deposit"
+                            sign = "+"
+                        
+                        elif t_type == "withdraw":
+                            details = "ATM Withdrawal"
+                            sign = "-"
+                        
+                        elif t_type == "transfer_out":
+                            recipient = tran["to"]
+                            details = f"To {recipient} ({accounts[recipient]['name']})"
+                            sign = "-"
+                            
+                        elif t_type == "transfer_in":
+                            sender = tran["from"]
+                            details = f"From {sender} ({accounts[sender]['name']})"
+                            sign = "+"
+                        
+                        print(f"{t_id:<10}{t_type.upper():<20}{details:<30}{sign}{amount:<15.2f}{time}")
+                            
+                            
                         
                     
                     
